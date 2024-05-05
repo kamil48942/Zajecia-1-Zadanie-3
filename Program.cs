ffbf1a9a91542fbc216dbe476c7763baab4b110a
Lenovo <Lenovo@DESKTOP-81DQNOK>
2024-05-05 15:12:52 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nazwaPliku = "plikdoodczytu.txt";
+
+        try
+        {
+            using (FileStream fs = new FileStream(nazwaPliku, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string zawartosc = sr.ReadToEnd();
+                    Console.WriteLine("Zawartość pliku:");
+                    Console.WriteLine(zawartosc);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił błąd: " + ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
